6358e5be468f30871e7aa0d6a8de97948836d61b
dozenji <douglasg.moore@gmail.com>
2019-05-01 10:49:32 -0500

    plaing with querySelect and querySelectAll

---
+++ main.js
@@ -0,0 +1,23 @@
+/* collect global variable*/
+let name = prompt('what is your name?');
+
+/* function to error check name*/
+function errorCheck(name){
+    if(name === ''){
+    /* if the name is blank ask again and overwrite the old value with the new one*/
+        name = prompt("No really,  what's your name?");
+        /* RECURSION fxn calls itself again*/
+        name = errorCheck(name);
+    }
+
+    return name;
+}
+/* capture what is returned by checkname and return that value*/
+name = errorCheck(name);
+
+let output = document.querySelectorAll();
+
+output.textContent = 'Thanks for visiting, ' + name + '.';
+/* Functional Expression
+          let errorCheck = function (){};
+          */
