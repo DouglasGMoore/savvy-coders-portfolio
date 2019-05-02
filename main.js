9cd53d15a28579249c88a3e8bd892844e0e3194e
dozenji <douglasg.moore@gmail.com>
2019-05-01 19:29:16 -0500

    refactor greeting

--- main.js
+++ main.js
@@ -2,20 +2,21 @@
 let name = prompt('what is your name?');
 
 /* function to error check name*/
-function errorCheck(name){
-    if(name === ''){
+function checkName(name){
+    /* Use !name to check if name has a value. !name is false if the string is empty */    
+    if(!name){
     /* if the name is blank ask again and overwrite the old value with the new one*/
         name = prompt("No really,  what's your name?");
         /* RECURSION fxn calls itself again*/
-        name = errorCheck(name);
+        name = checkName(name);
     }
 
     return name;
 }
 /* capture what is returned by checkname and return that value*/
-name = errorCheck(name);
+name = checkName(name);
 
-let output = document.querySelector('#greeting');
+let output = document.querySelector('h1');
 
 output.textContent = 'Thanks for visiting, ' + name + '.';
 /* Functional Expression
