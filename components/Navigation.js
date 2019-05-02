b50b391d86aa85aff146e2dd50319b1dcab9de2d
dozenji <douglasg.moore@gmail.com>
2019-05-01 21:11:42 -0500

    add imports and exports to components

---
+++ components/Navigation.js
@@ -0,0 +1,17 @@
+export default `<nav>
+<ul>
+ <li class="portfolio"><a href="portfolio.html">Portfolio</a>
+     <ul id="dropdown">
+         <li><a href="portfolio.html">HTML/CSS</a></li>
+         <li><a href="portfolio.html">JavaScript</a></li>
+         <li><a href="DemoDay.html">Class Showcase</a></li>
+         <li><a href="DemoDay.html">Web Store</a></li>
+         <li><a href="DemoDay.html">Demo Day Project</a></li>
+     </ul>
+ </li>
+ <li class="welcome"><a class="welcome"href="index.html">Welcome</a></li>
+ <li class="contact"><a class="contact" href="contact.html">Contact Me</a></li>
+ <li class="about"><a class="about" href="about.html">About Me</a></li>
+</ul>
+</nav>
+`;
---
+++ index.js
@@ -0,0 +1,6 @@
+import Navigation from './Navigation';
+import Animation from './Animation';
+import Main from './Main';
+import Footer from './Footer';
+
+console.log(Navigation, Animation, Main, Footer)
