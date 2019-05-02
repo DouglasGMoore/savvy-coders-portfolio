e14016cb536467e5cf66f9d32d859750f4e52781
dozenji <douglasg.moore@gmail.com>
2019-05-01 20:49:16 -0500

    add notes and created modules

--- main.js
+++ main.js
@@ -1,12 +1,18 @@
-/* collect global variable*/
-let name = prompt('what is your name?');
+/* create global variable*/
+let nameArr = []; // creates nameArr as an empty array//
+
+nameArr[0] = prompt('what is your first name?');
+nameArr[1] = prompt('what is your last name?');
 
 /* function to error check name*/
 function checkName(name){
-    /* Use !name to check if name has a value. !name is false if the string is empty */    
-    if(!name){
-    /* if the name is blank ask again and overwrite the old value with the new one*/
-        name = prompt("No really,  what's your name?");
+    /* Use !name to check if name has a value. !name is false if the string is empty */
+
+    if(!name[0] || !name[1]){
+    /* if either name is blank ask again and overwrite the old value with the new one*/
+        nameArr[0] = prompt("No really,  what's your first name?");
+        nameArr[1] = prompt("No really,  what's your last name?");
+
         /* RECURSION fxn calls itself again*/
         name = checkName(name);
     }
@@ -14,13 +20,15 @@ function checkName(name){
     return name;
 }
 /* capture what is returned by checkname and return that value*/
-name = checkName(name);
+nameArr = checkName(nameArr);
 
 let output = document.querySelector('#greeting');
 
-output.innerHTML = `${name}`;
+output.innerHTML = `<h1>${nameArr[0]} ${nameArr[1]}</h1>`;
 // output.textContent = `<p>${name}</p>`;
 
 /* Functional Expression
-          let errorCheck = function (){};
+          let errorCheck = function() {
+              code
+          };
           */
