319d336f7ff60a34ce56a3403eee740eeb0a1e58
dozenji <49459156+dozenji@users.noreply.github.com>
2019-05-04 15:30:52 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
+<<<<<<< HEAD
+import Navigation from './components/Navigation';
+import Animation from './components/Animation';
+import Main from './components/Main';
+import Footer from './components/Footer';
+
+
+console.log('hello');
+console.log(Navigation, Animation, Main, Footer);
+
+document.body.innerHTML = `
+${Navigation}
+${Animation}
+${Main}
+${Footer}
+`;
+=======
 import Navigation from './Navigation';
 import Animation from './Animation';
 import Main from './Main';
@@ -11,3 +28,4 @@ ${Cntent}
 ${Main}
 ${Footer}
 `;
+>>>>>>> 0865bebdbd807534d2c26af14b0fa30b7494cf3b
