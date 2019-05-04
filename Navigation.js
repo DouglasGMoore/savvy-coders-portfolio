1e7136bb5ec0c1ea8841e045571b21055d445a62
dozenji <49459156+dozenji@users.noreply.github.com>
2019-05-04 15:29:23 -0500

    Add files via upload

---
+++ Navigation.js
@@ -0,0 +1,29 @@
+<<<<<<< HEAD
+export default `
+<div class="container">
+<div class="header">
+          
+<nav>       
+=======
+export default `<nav>
+>>>>>>> 0865bebdbd807534d2c26af14b0fa30b7494cf3b
+<ul>
+ <li class="portfolio"><a href="portfolio.html">Portfolio</a>
+     <ul id="dropdown">
+         <li><a href="portfolio.html">HTML/CSS</a></li>
+         <li><a href="portfolio.html">JavaScript</a></li>
+         <li><a href="DemoDay.html">Class Showcase</a></li>
+         <li><a href="DemoDay.html">Web Store</a></li>
+         <li><a href="DemoDay.html">Demo Day Project</a></li>
+     </ul>
+ </li>
+ <li class="welcome"><a class="welcome"href="index.html">Welcome</a></li>
+ <li class="contact"><a class="contact" href="contact.html">Contact Me</a></li>
+ <li class="about"><a class="about" href="about.html">About Me</a></li>
+</ul>
+</nav>
+<<<<<<< HEAD
+</div>
+=======
+>>>>>>> 0865bebdbd807534d2c26af14b0fa30b7494cf3b
+`;
