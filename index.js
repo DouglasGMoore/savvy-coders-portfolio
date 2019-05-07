b64e16c5d8b93eade677c091c251592d4607e636
dozenji <douglasg.moore@gmail.com>
2019-05-06 19:20:22 -0500

    parcel error fix

--- index.js
+++ index.js
@@ -3,14 +3,10 @@ import Animation from './components/Animation';
 import Main from './components/Main';
 import Footer from './components/Footer';
 
-
-console.log('hello');
-console.log(Navigation, Animation, Main, Footer);
-
-document.body.innerHTML = `
+document.querySelector('#root').innerHTML = 
+`
 ${Navigation}
 ${Main}
 ${Animation}
-
 ${Footer}
 `;
